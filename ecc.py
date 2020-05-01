d6beccf736255fb60c1aa5904af8e9b311cc9358
bejitono <stdemicheli@protonmail.com>
2020-05-01 14:54:52 +0700

    Adds 256Field/Point, Signature, Private Key.

--- ecc.py
+++ ecc.py
@@ -1,4 +1,8 @@
+from random import randint
 from unittest import TestCase
+
+import hashlib
+import hmac
 
 class FieldElement:
 
@@ -145,6 +149,17 @@ class Point:
             y = s * (self.x - x) - self.y
             return self.__class__(x, y, self.a, self.b)
 
+    def __rmul__(self, coefficient):
+        coef = coefficient
+        current = self
+        result = self.__class__(None, None, self.a, self.b)
+        while coef:
+            if coef & 1:
+                result += current
+            current += current
+            coef >>= 1
+        return result
+
 class PointTest(TestCase):
 
     def test_ne(self):
@@ -152,6 +167,13 @@ class PointTest(TestCase):
         b = Point(x=18, y=77, a=5, b=7)
         self.assertTrue(a != b)
         self.assertFalse(a != a)
+
+    def test_on_curve(self):
+        with self.assertRaises(ValueError):
+            Point(x=-2, y=4, a=5, b=7)
+        # these should not raise an error
+        Point(x=3, y=-7, a=5, b=7)
+        Point(x=18, y=77, a=5, b=7)
 
     def test_add0(self):
         a = Point(x=None, y=None, a=5, b=7)
@@ -167,5 +189,229 @@ class PointTest(TestCase):
         self.assertEqual(a + b, Point(x=2, y=-5, a=5, b=7))
 
     def test_add2(self):
-        a = Point(x=-1, y=-1, a=5, b=7)
-        self.assertEqual(a + a, Point(x=18, y=77, a=5, b=7))
+        a = Point(x=-1, y=1, a=5, b=7)
+        self.assertEqual(a + a, Point(x=18, y=-77, a=5, b=7))
+        
+class ECCTest(TestCase):
+
+    def test_on_curve(self):
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(7, prime)
+        valid_points = ((192, 105), (17, 56), (1, 193))
+        invalid_points = ((200, 119), (42, 99))
+        for x_raw, y_raw in valid_points:
+            x = FieldElement(x_raw, prime)
+            y = FieldElement(y_raw, prime)
+            Point(x, y, a, b)  # <1>
+        for x_raw, y_raw in invalid_points:
+            x = FieldElement(x_raw, prime)
+            y = FieldElement(y_raw, prime)
+            with self.assertRaises(ValueError):
+                Point(x, y, a, b)  # <1>
+    # end::source2[]
+
+    def test_add(self):
+        # tests the following additions on curve y^2=x^3-7 over F_223:
+        # (192,105) + (17,56)
+        # (47,71) + (117,141)
+        # (143,98) + (76,66)
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(7, prime)
+
+        additions = (
+            # (x1, y1, x2, y2, x3, y3)
+            (192, 105, 17, 56, 170, 142),
+            (47, 71, 117, 141, 60, 139),
+            (143, 98, 76, 66, 47, 71),
+        )
+
+        # loop over additions
+        # initialize x's and y's as FieldElements
+        # create p1, p2 and p3 as Points
+        # check p1+p2==p3
+        raise NotImplementedError
+
+    def test_rmul(self):
+        # tests the following scalar multiplications
+        # 2*(192,105)
+        # 2*(143,98)
+        # 2*(47,71)
+        # 4*(47,71)
+        # 8*(47,71)
+        # 21*(47,71)
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(7, prime)
+
+        multiplications = (
+            # (coefficient, x1, y1, x2, y2)
+            (2, 192, 105, 49, 71),
+            (2, 143, 98, 64, 168),
+            (2, 47, 71, 36, 111),
+            (4, 47, 71, 194, 51),
+            (8, 47, 71, 116, 55),
+            (21, 47, 71, None, None),
+        )
+
+        # iterate over the multiplications
+        for s, x1_raw, y1_raw, x2_raw, y2_raw in multiplications:
+            x1 = FieldElement(x1_raw, prime)
+            y1 = FieldElement(y1_raw, prime)
+            p1 = Point(x1, y1, a, b)
+            # initialize the second point based on whether it's the point at infinity
+            if x2_raw is None:
+                p2 = Point(None, None, a, b)
+            else:
+                x2 = FieldElement(x2_raw, prime)
+                y2 = FieldElement(y2_raw, prime)
+                p2 = Point(x2, y2, a, b)
+
+            # check that the product is equal to the expected point
+            self.assertEqual(s * p1, p2)
+
+# parameters for secp256k1
+A = 0
+B = 7
+P = 2**256 - 2**32 - 977
+N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+
+
+class S256Field(FieldElement):
+
+    def __init__(self, num, prime=None):
+        super().__init__(num=num, prime=P)
+
+    def __repr__(self):
+        return '{:x}'.format(self.num).zfill(64)
+
+
+class S256Point(Point):
+
+    def __init__(self, x, y, a=None, b=None):
+        a, b = S256Field(A), S256Field(B)
+        if type(x) == int:
+            super().__init__(x=S256Field(x), y=S256Field(y), a=a, b=b)
+        else:
+            super().__init__(x=x, y=y, a=a, b=b)  # <1>
+
+    def __repr__(self):
+        if self.x is None:
+            return 'S256Point(infinity)'
+        else:
+            return 'S256Point({}, {})'.format(self.x, self.y)
+
+    def __rmul__(self, coefficient):
+        coef = coefficient % N
+        return super().__rmul__(coef)
+
+    def verify(self, z, sig):
+        s_inv = pow(sig.s, N - 2, N)  # <1>
+        u = z * s_inv % N  # <2>
+        v = sig.r * s_inv % N  # <3>
+        total = u * G + v * self  # <4>
+        return total.x.num == sig.r  # <5>
+
+
+G = S256Point(
+    0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+    0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
+
+
+class S256Test(TestCase):
+
+    def test_order(self):
+        point = N * G
+        self.assertIsNone(point.x)
+
+    def test_pubpoint(self):
+        # write a test that tests the public point for the following
+        points = (
+            # secret, x, y
+            (7, 0x5cbdf0646e5db4eaa398f365f2ea7a0e3d419b7e0330e39ce92bddedcac4f9bc, 0x6aebca40ba255960a3178d6d861a54dba813d0b813fde7b5a5082628087264da),
+            (1485, 0xc982196a7466fbbbb0e27a940b6af926c1a74d5ad07128c82824a11b5398afda, 0x7a91f9eae64438afb9ce6448a1c133db2d8fb9254e4546b6f001637d50901f55),
+            (2**128, 0x8f68b9d2f63b5f339239c1ad981f162ee88c5678723ea3351b7b444c9ec4c0da, 0x662a9f2dba063986de1d90c2b6be215dbbea2cfe95510bfdf23cbf79501fff82),
+            (2**240 + 2**31, 0x9577ff57c8234558f293df502ca4f09cbc65a6572c842b39b366f21717945116, 0x10b49c67fa9365ad7b90dab070be339a1daf9052373ec30ffae4f72d5e66d053),
+        )
+
+        # iterate over points
+        for secret, x, y in points:
+            # initialize the secp256k1 point (S256Point)
+            point = S256Point(x, y)
+            # check that the secret*G is the same as the point
+            self.assertEqual(secret * G, point)
+
+    def test_verify(self):
+        point = S256Point(
+            0x887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c,
+            0x61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34)
+        z = 0xec208baa0fc1c19f708a9ca96fdeff3ac3f230bb4a7ba4aede4942ad003c0f60
+        r = 0xac8d1c87e51d0d441be8b3dd5b05c8795b48875dffe00b7ffcfac23010d3a395
+        s = 0x68342ceff8935ededd102dd876ffd6ba72d6a427a3edb13d26eb0781cb423c4
+        self.assertTrue(point.verify(z, Signature(r, s)))
+        z = 0x7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d
+        r = 0xeff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c
+        s = 0xc7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6
+        self.assertTrue(point.verify(z, Signature(r, s)))
+
+
+class Signature:
+
+    def __init__(self, r, s):
+        self.r = r
+        self.s = s
+
+    def __repr__(self):
+        return 'Signature({:x},{:x})'.format(self.r, self.s)
+
+
+class PrivateKey:
+
+    def __init__(self, secret):
+        self.secret = secret
+        self.point = secret * G  # <1>
+
+    def hex(self):
+        return '{:x}'.format(self.secret).zfill(64)
+    # end::source13[]
+
+    # tag::source14[]
+    def sign(self, z):
+        k = self.deterministic_k(z)  # <1>
+        r = (k * G).x.num
+        k_inv = pow(k, N - 2, N)
+        s = (z + r * self.secret) * k_inv % N
+        if s > N / 2:
+            s = N - s
+        return Signature(r, s)
+
+    def deterministic_k(self, z):
+        k = b'\x00' * 32
+        v = b'\x01' * 32
+        if z > N:
+            z -= N
+        z_bytes = z.to_bytes(32, 'big')
+        secret_bytes = self.secret.to_bytes(32, 'big')
+        s256 = hashlib.sha256
+        k = hmac.new(k, v + b'\x00' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        k = hmac.new(k, v + b'\x01' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        while True:
+            v = hmac.new(k, v, s256).digest()
+            candidate = int.from_bytes(v, 'big')
+            if candidate >= 1 and candidate < N:
+                return candidate  # <2>
+            k = hmac.new(k, v + b'\x00', s256).digest()
+            v = hmac.new(k, v, s256).digest()
+    # end::source14[]
+
+
+class PrivateKeyTest(TestCase):
+
+    def test_sign(self):
+        pk = PrivateKey(randint(0, N))
+        z = randint(0, 2**256)
+        sig = pk.sign(z)
+        self.assertTrue(pk.point.verify(z, sig))
